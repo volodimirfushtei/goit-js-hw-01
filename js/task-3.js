2f5b0b76447e735d36a775fe8c261facc34932ef
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-05-15 16:11:04 +0300

    add task 1,2,3

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+function getElementWidth(content, padding, border) {
+  content === 'Npx';
+  padding === 'Npx';
+  border === 'Npx';
+  const product =
+    parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
+  return product;
+}
+console.log(getElementWidth('50px', '8px', '4px'));
+console.log(getElementWidth('60px', '12px', '8.5px'));
+console.log(getElementWidth('200px', '0px', '0px'));
